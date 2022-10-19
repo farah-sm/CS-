4eeb9e6e1806cb8f6b176556289326dbcf458ce6
Farah s <65547726+farah-sm@users.noreply.github.com>
2022-10-19 10:54:20 +0100

    Create SegmentClass-Shapes.cs

---
+++ S3/SegmentClass-Shapes.cs
@@ -0,0 +1,26 @@
+using System;
+
+
+namespace Shapes
+{
+    internal class Segment
+    {
+
+        private int x;
+        private int y;
+
+        public Segment(int xarg, int yarg)
+        {
+            x = xarg;
+            y = yarg;
+        }
+
+        public void Length()
+        {
+
+          
+            double legnth = p1.DistanceFrom
+
+        }
+    }
+}
