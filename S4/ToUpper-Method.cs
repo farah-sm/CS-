f0bda0ee39bbe63fa324d639381f12c62b18de4a
Farah s <65547726+farah-sm@users.noreply.github.com>
2022-10-26 10:23:45 +0100

    Create ToUpper-Method.cs

---
+++ S4/ToUpper-Method.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Week_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] array = { 'a', 'b', 'c', 'd', 'e', 'f' };
+            string s2 = new string(array);
+
+
+
+            Console.WriteLine(s2);
+            Console.WriteLine(s2.ToUpper());
+
+
+
+        }
+    }
+}
