33a3c5334dcba13d74ed1f557055c3c2424123df
Farah s <65547726+farah-sm@users.noreply.github.com>
2022-11-16 09:54:42 +0000

    Update Class-Address(person).cs

--- S6/Class-Address(person).cs
+++ S6/Class-Address(person).cs
@@ -1,1 +1,35 @@
 
+using System;
+namespace AddressProject
+{
+    public class Address
+    {
+        private int number;
+        private string street;
+        private string city;
+        private string postcode;
+        public Address(string address)
+        {
+            string[] tokens = address.Split(";");
+            string firstLine = tokens[0];
+            string[] firstLineTokens = firstLine.Split(" ");
+            number = Convert.ToInt32(firstLineTokens[0]);
+            street = firstLineTokens[1];
+            city = tokens[1];
+            postcode = tokens[2];
+            // checks isValid and raise an error in case
+        }
+        // should check whether it is a valid address
+        // always returns true for now
+        private bool IsValid()
+        {
+            return true;
+        }
+        public override string ToString()
+        {
+            return $"Number: {number}, Street: {street}, City: {city}, Postcode: {postcode}\n";
+        }
+    }
+}
+
+// To make a project reference, right click over the project and click "Add" then click "Project Reference" and click the project you'd like tor reference
